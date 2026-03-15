91ff81ce271745761dd3c86d05b4ad6465eea66f
cumali-sari <sariczx@gmail.com>
2026-03-15 11:35:36 +0300

    Add main.py

---
+++ app/models.py
@@ -0,0 +1,19 @@
+from sqlalchemy import Column, Integer, String
+from database import Base, engine
+
+
+class User(Base):
+    __tablename__ = "users"
+    id = Column(Integer, primary_key=True)
+    email= Column(String)
+    password = Column(String)
+
+class Resource(Base):
+    __tablename__= "resources"
+    id= Column(Integer, primary_key= True)
+    title = Column(String)
+    url = Column(String)
+    tags = Column(String)
+    owner_id = Column(String)
+
+Base.metadata.create_all(bind=engine)
