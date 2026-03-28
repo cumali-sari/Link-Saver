64008bcdf5360628e26a05d502acccc8fb9e045f
cumali-sari <sariczx@gmail.com>
2026-03-28 09:37:59 +0300

    Add main page

--- app/main.py
+++ app/main.py
@@ -1,7 +1,8 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Request
 from app.routers import resources, users
 from fastapi.staticfiles import StaticFiles
-
+from fastapi.templating import Jinja2Templates
+from pathlib import Path
 
 
 app = FastAPI()
@@ -9,6 +10,9 @@ app.mount("/static", StaticFiles(directory="app/static"), name="static")
 app.include_router(resources.router)
 app.include_router(users.router)
 
+BASE_DIR= Path(__file__).resolve().parent
+templates= Jinja2Templates(directory=str(BASE_DIR/ "templates"))
+
 @app.get("/")
-def main_page():
-    return "Hello"
+def resources_page(request: Request):
+    return templates.TemplateResponse("index.html", {"request": request})
