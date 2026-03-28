cec15ead6ef94d6e6a587722af57fdb2cc58e770
Cumali Sari <sariczx@gmail.com>
2026-03-28 12:03:19 +0300

    Update main.py

--- app/main.py
+++ app/main.py
@@ -4,14 +4,14 @@ from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
 from pathlib import Path
 
-
 app = FastAPI()
-app.mount("/static", StaticFiles(directory="app/static"), name="static")
+
+BASE_DIR = Path(__file__).resolve().parent
+templates = Jinja2Templates(directory=BASE_DIR / "templates")
+app.mount("/static", StaticFiles(directory=BASE_DIR / "static"), name="static")
+
 app.include_router(resources.router)
 app.include_router(users.router)
-
-BASE_DIR= Path(__file__).resolve().parent
-templates= Jinja2Templates(directory=str(BASE_DIR/ "templates"))
 
 @app.get("/")
 def main_page(request: Request):
