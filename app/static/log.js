178d9b9aa532e363c84c5a17cb06dcb9abe110f7
Cumali Sari <sariczx@gmail.com>
2026-03-28 12:47:26 +0300

    Minor tweaks

--- app/static/log.js
+++ app/static/log.js
@@ -8,7 +8,7 @@ login_form.addEventListener("submit", async (e) => {
 
     
 
-    const res = await fetch("http://127.0.0.1:8000/login", {
+    const res = await fetch("/login", {
         method: "POST",
         headers: {
             "Content-Type": "application/json"
